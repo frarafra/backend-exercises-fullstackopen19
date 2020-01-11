2067217fec77a9ffbfa88a01a920999b5cc10970
Francesc Ara <cesc78@gmail.com>
2020-01-11 19:13:37 +0100

    Add environmental variables file

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const routes = require('./routes')
 
-const PORT = 3001
+const PORT = process.env.PORT
 
 const app = express()
 
@@ -14,6 +15,7 @@ morgan.token('req-body', req => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
 app.use(bodyParser.json())
 app.use(cors())
+app.use(express.static('build'))
 app.use('/', routes)
 
 app.listen(PORT, () => {

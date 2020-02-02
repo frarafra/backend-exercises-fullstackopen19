bf944607cbf59a42963d0ebd4d5d48b962d3cbbd
Francesc Ara <cesc78@gmail.com>
2020-02-02 15:13:26 +0100

    uibuild

---
+++ build/precache-manifest.b67015282a067c9c077d66bc4dce3248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b581e16315406aa1b4130ca8f3106a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce108285732e70289f6c",
+    "url": "/static/css/main.f2fef06b.chunk.css"
+  },
+  {
+    "revision": "1d1b449f9ac57f68ba69",
+    "url": "/static/js/2.9c5f27b7.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.9c5f27b7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce108285732e70289f6c",
+    "url": "/static/js/main.1317b588.chunk.js"
+  },
+  {
+    "revision": "4230d87a2d3cbabbdf4d",
+    "url": "/static/js/runtime-main.e53b3d9b.js"
+  }
+]);
